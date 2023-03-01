cbf8e4c40d9f6d64f5bc37353f7994eaecf7075b
KAKAN <22027824+theKAKAN@users.noreply.github.com>
2023-03-02 00:29:09 +0530

    Modify SearchByTitle to return a custom result

--- src/main/java/api/SearchByTitle.java
+++ src/main/java/api/SearchByTitle.java
@@ -12,12 +12,31 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+// For storing objects and comparing
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+// For JSON parsing
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Servlet implementation class SearchByTitle
  */
 public class SearchByTitle extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+    static class Book {
+        String title;
+        String publishedDate;
+        String publisher;
+
+        public Book(String title, String publishedDate, String publisher) {
+            this.title = title;
+            this.publishedDate = publishedDate;
+            this.publisher = publisher;
+        }
+    }
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -32,8 +51,8 @@ public class SearchByTitle extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		// Search using Google Books API for title
-		String title = request.getParameter("title");
-        String encodedAuthor = URLEncoder.encode(title, "UTF-8");
+		String titleParam = request.getParameter("title");
+        String encodedAuthor = URLEncoder.encode(titleParam, "UTF-8");
         URL url = new URL("https://www.googleapis.com/books/v1/volumes?q=intitle:" + encodedAuthor);
 
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -46,12 +65,61 @@ public class SearchByTitle extends HttpServlet {
         
         BufferedReader br = new BufferedReader(new InputStreamReader((connection.getInputStream())));
         
-        // Write the response
-        response.setContentType("application/json");
-        String output;
-        while ((output = br.readLine()) != null) {
-            response.getWriter().write(output);
+        StringBuilder res = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            res.append(line);
         }
+        br.close();
+
+        JSONObject jsonObject = new JSONObject(res.toString());
+        JSONArray items = jsonObject.getJSONArray("items");
+
+        ArrayList<Book> books = new ArrayList<>();
+        for (int i = 0; i < items.length(); i++) {
+            JSONObject item = items.getJSONObject(i);
+            JSONObject volumeInfo = item.getJSONObject("volumeInfo");
+
+            String title = volumeInfo.getString("title");
+            String publishedDate = volumeInfo.optString("publishedDate", "");
+            String publisher = volumeInfo.optString("publisher", "");
+
+            Book book = new Book(title, publishedDate, publisher);
+            books.add(book);
+        }
+
+        // Sort the books by year published (in descending order)
+        Collections.sort(books, new Comparator<Book>() {
+            @Override
+            public int compare(Book b1, Book b2) {
+                if (b1.publishedDate == null && b2.publishedDate == null) {
+                    return 0;
+                } else if (b1.publishedDate == null) {
+                    return 1;
+                } else if (b2.publishedDate == null) {
+                    return -1;
+                } else {
+                    return b2.publishedDate.compareTo(b1.publishedDate);
+                }
+            }
+        });
+
+        // Convert it into JSON again
+        JSONArray resultArray = new JSONArray();
+        for (Book book : books) {
+            JSONObject bookObj = new JSONObject();
+            bookObj.put("title", book.title);
+            bookObj.put("publishedDate", book.publishedDate);
+            bookObj.put("publisher", book.publisher);
+            resultArray.put(bookObj);
+        }
+
+        JSONObject result = new JSONObject();
+        result.put("books", resultArray);
+                
+        // Set response type
+        response.setContentType("application/json");
+        response.getWriter().write(result.toString());
 
         connection.disconnect();
 	}
